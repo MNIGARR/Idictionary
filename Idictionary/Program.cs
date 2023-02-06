6c451a87e9609029f276b7622e8e8ad814b62bd6
admin <admin@DESKTOP-D6KE8CP>
2023-02-07 03:46:27 +0400

    Add project files.

---
+++ Idictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Example
+{
+    public static void Main()
+    {
+
+        //IDictionary<string, string> dict = new Dictionary<string, string>();
+
+        //dict.Add("Nigar", "Mustafazada");
+        //dict.Add("Aysu", "Quliyeva");
+        //dict.Add("Ayshan", "Oruclu");
+        //dict.Add("Xayyam", "Mammadov");
+        //foreach (var item in dict)
+        //{
+        //    Console.WriteLine(item);
+        //}
+        //dict.Remove("Xayyam");
+        //Console.WriteLine();
+        //foreach (var item in dict)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+        //////////////////////////////////////////////////////
+
+        //SortedList<int, string> dict2 = new();
+
+        //dict2.Add(2, "Aysu");
+        //dict2.Add(1, "Nigar");
+
+        //Console.WriteLine("Sorted List: ");
+
+        //foreach (var item in dict2)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+
+    }
+}
